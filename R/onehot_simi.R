9cdaab275072ec6bfe3b578936fd2f939a0e93b5
Lingshu Hu <lingshu.hu@hotmail.com>
2021-02-05 18:39:18 -0600

    update

--- R/onehot_simi.R
+++ R/onehot_simi.R
@@ -13,4 +13,11 @@ d <- quanteda::dfm(df$text)
 this <- quanteda::dfm_select(d, "this")
 
 quanteda::textstat_simil(d[, c("this", "is")],  method = "cosine", margin = "features")
-                         
+
+
+## read data
+df <- rtweet::read_twitter_csv("data/covid_usa_jan24-mar10_21pm-23pm_onethird_text_clean.csv")
+
+dv <- quanteda::dfm(df$text.1)
+s <- quanteda::textstat_simil(dv[, c("covid19", "china")],  method = "cosine", margin = "features")
+s@x[2] ## extract the similarity score
