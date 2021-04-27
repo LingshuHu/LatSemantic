3419a433fb0e58803231e92fbc8903f3173e37d5
Lingshu Hu <lingshu.hu@hotmail.com>
2021-04-27 11:06:01 -0500

    update

--- R/examine_text.R
+++ R/examine_text.R
@@ -12,3 +12,26 @@ dfs <- df2[grepl("radiation", df2$textp)&grepl("covid19", df2$textp),]
 dfs <- dfs[!duplicated(dfs$textp), ]
 txt <- data.frame(id = nrow(dfs), text = dfs$text)
 write.csv(txt, "results/hoax&covid.csv")
+
+
+
+#####
+files <- list.files("data_week/usa", full.names = T)
+
+key_sample <- vector("list", length = length(files))
+for (i in seq_along(files)) {
+  df <- rtweet::read_twitter_csv(files[i])
+  keydf <- df[grepl("flu", df$text, ignore.case = T)& #frontlines?; 5G; fight; hoax; flu
+                grepl("covid19|covid|coronavirus", df$text, ignore.case = T),]
+  if (nrow(keydf) > 1000) {
+    set.seed(123)
+    keydf <- dplyr::sample_n(keydf, 1000)
+  }
+  key_sample[[i]] <- keydf
+}
+
+key_sample <- do.call("rbind", key_sample)
+key_sample <- key_sample[!duplicated(key_sample$text), ]
+
+rtweet::write_as_csv(key_sample, "sample_texts/flu_week1-10.csv") # frontline; 5g; fight; hoax; flu
+rtweet::write_as_csv(key_sample[, 1:5], "sample_texts/flu_week1-10_texts.csv")
