1eaa4630777358a084437b61d3df196bcbae57f8
Lingshu Hu <lingshu.hu@hotmail.com>
2021-07-24 20:13:53 -0500

    update

--- R/analyze_similarity.R
+++ R/analyze_similarity.R
@@ -1,44 +1,69 @@
 
-###################### find frequency of metaphor and misinfo words ################
+###################### find frequency and similarity of metaphor and misinfo words ################
 #install.packages("jsonlite")
-## function to count words matches in a dictionary
-word_match <- function(x, dict) {
-  if (is.character(x)) {
-    ## this removes URLs
-    #x <- gsub("https?://\\S+|@\\S+", "", x)
-    x <- tokenizers::tokenize_words(
-      x, lowercase = TRUE, strip_punct = TRUE, strip_numeric = FALSE
-    )
-  }
-  word_count <- function(token) {
-    total_words_count <- length(token)
-    med_words_count <- sum(dict$value[match(token, dict$word)], na.rm = TRUE)
-    med_words_ratio <- med_words_count/total_words_count
-    data.frame(total_words_count = total_words_count,
-               med_words_count = med_words_count,
-               stringsAsFactors = FALSE)
-  }
-  #count <- lapply(x, word_count)
-  #count <- do.call("rbind", count)
-  count <- word_count(x)
-}
-
-
 kw <- jsonlite::fromJSON("data/all_keywords_pairs.json", flatten = T)
-kw <- unique(kw[,1])
-kw <- data.frame(word = kw, value = rep(1, length(kw)))
+met <- kw[1:101, 1]
+met <- unique(met)
+mis1 <- kw[102:179, 1]
+mis2 <- kw[116:179, 2]
+mis <- unique(c(mis1, mis2))
+
+simi <- read.csv("results/simi_usa01-52_word2vec_non-neg.csv", stringsAsFactors = F)
+colnames(simi) <- c("word1", "word2", paste0("week", 1:52))
 
 
-files <- list.files("data_week_text/usa", full.names = T)
-tt <- readLines(files[1])
-stringr::str_count(tt[1], "co")
-tt1 <- stringr::str_c(tt, collapse = " ")
+fs <- list.files("results/usa", full.names = T)
 
-frq <- vector("list", length = nrow(kw))
-for (i in seq_along(kw$word)) {
-  frq[[i]] <- word_match(tt1, kw[i, ])
+met_freq <- vector("list", length = length(fs))
+mis_freq <- vector("list", length = length(fs))
+for (i in seq_along(fs)) {
+  df <- read.csv(fs[i], stringsAsFactors = F)
+  df[is.na(df)] <- 0
+  df$rate <- 100*df$frequency/df$total_words
+  met_df <- subset(df, words %in% met)
+  met_df <- dplyr::left_join(met_df, simi[1:101, c("word1", paste0("week", i))], by = c("words" = "word1"))
+  colnames(met_df)[6] <- "similarity"
+  met_freq[[i]] <- met_df[order(met_df$rate, decreasing = T), ]
 }
 
+met_freq <- do.call("cbind", met_freq)
+
+write.csv(met_freq, "results/top_met_words_by_weeks.csv", row.names = F)
+
+#######################
+## similarity among 5 top words
+simi <- read.csv("results/simi_usa01-52_word2vec_non-neg_all_connections.csv", stringsAsFactors = F)
+colnames(simi) <- c("word1", "word2", paste0("week", 1:52))
+simi$pairs <- paste(simi$word1, simi$word2, sep = "-")
+fs <- list.files("results/usa", full.names = T)
+
+top_met_simis <- vector("list", length = length(fs))
+#mis_freq <- vector("list", length = length(fs))
+for (i in seq_along(fs)) {
+  df <- read.csv(fs[i], stringsAsFactors = F)
+  df[is.na(df)] <- 0
+  df$rate <- 100*df$frequency/df$total_words
+  met_df <- subset(df, words %in% met)
+  met_df <- met_df[order(met_df$rate, decreasing = T), ]
+  met_df <- met_df[1:5, ]
+  w1 <- vector("list", length = 5)
+  for (x in seq_along(met_df$words)) {
+    w2 <- vector("list", length = 5)
+    for (y in seq_along(met_df$words[x:5])) {
+      w2[[y]] <- paste(met_df$words[x], met_df$words[x:5][y], sep = "-")
+    }
+    w1[[x]] <- unlist(w2) 
+  }
+  pairs <- data.frame(pairs = unlist(w1))
+  pairs <- dplyr::left_join(pairs, simi[, c("pairs", paste0("week", i))], by = "pairs")
+  #colnames(pairs)[6] <- "similarity"
+  top_met_simis[[i]] <- pairs
+}
+
+top_met_simis <- do.call("cbind", top_met_simis)
+write.csv(top_met_simis, "results/top_met_words_by_weeks_all_connections.csv", row.names = F)
+
+#####################################
 ## read data
 df <- read.csv("results/simi_usa01-52_word2vec_non-neg.csv")
 weeks <- unlist(lapply(1:52, function(x) paste0("week", x)))
